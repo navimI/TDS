ac92bbdd21e8d79a9f761f9f0f6736c3afe68428
LolaDSI <LolaDSI@DESKTOP-V09ODBD>
2024-06-09 18:35:42 +0200

    Pulsador Luz Maven

---
+++ MavenPulsadorLuzProject/src/main/java/pulsador/IEncendidoListener.java
@@ -0,0 +1,8 @@
+package pulsador;
+
+import java.util.EventListener;
+import java.util.EventObject;
+
+public interface IEncendidoListener extends EventListener{
+	public void enteradoCambioEncendido(EventObject e);
+}
